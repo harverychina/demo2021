6178c33c32f1904ecca83734ba4d729a27be2ac3
harverychina <harverychina@126.com>
2021-05-20 16:16:37 +0800

    update 0520

---
+++ src/demo32/com/awtDemo/awtDemo1.java
@@ -0,0 +1,12 @@
+package demo32.com.awtDemo;
+
+import java.awt.*;
+
+public class awtDemo1 {
+    public static void main(String[] args) {
+        Frame f = new Frame("我是窗体");
+        f.setSize(200, 300);
+        f.setVisible(true);
+
+    }
+}
